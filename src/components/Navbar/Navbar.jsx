03fdfc386f839d75aa7c5ff4813cf7a4f81e584c
Sebastian <sebastiandelescabe@gmail.com>
2022-02-25 00:25:09 -0300

    fix deploy 2.0

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,11 +15,11 @@ const Navbar = () => {
         <img src={images.gericht} alt="app logo" />
       </div>
       <ul className='app__navbar-links'>
-        <li className='p__opensans'><a href='#home'></a>Home</li> 
-        <li className='p__opensans'><a href='#about'></a>About</li>
-        <li className='p__opensans'><a href='#menu'></a>Menu</li>
-        <li className='p__opensans'><a href='#awards'></a>Awards</li>
-        <li className='p__opensans'><a href='#contact'></a>Contact</li>
+        <li className='p__opensans'><a href='#home'>Home</a></li> 
+        <li className='p__opensans'><a href='#about'>About</a></li>
+        <li className='p__opensans'><a href='#menu'>Menu</a></li>
+        <li className='p__opensans'><a href='#awards'>Awards</a></li>
+        <li className='p__opensans'><a href='#contact'>Contact</a></li>
       </ul>
       <div className='app__navbar-login'>
         <a href="#login" className='p__opensans'>Log In / Register</a>
@@ -33,11 +33,11 @@ const Navbar = () => {
             <div className='app__navbar-smallscreen-overlay flex__center slide-bottom'>
               <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
               <ul className='app__navbar-smallscreen-links'>
-                <li className='p__opensans'><a href='#home'></a>Home</li>
-                <li className='p__opensans'><a href='#about'></a>About</li>
-                <li className='p__opensans'><a href='#menu'></a>Menu</li>
-                <li className='p__opensans'><a href='#awards'></a>Awards</li>
-                <li className='p__opensans'><a href='#contact'></a>Contact</li>
+                <li className='p__opensans'><a href='/'>Home</a></li>
+                <li className='p__opensans'><a href='/'>About</a></li>
+                <li className='p__opensans'><a href='/'>Menu</a></li>
+                <li className='p__opensans'><a hhref='/'>Awards</a></li>
+                <li className='p__opensans'><a href='/'>Contact</a></li>
               </ul>
             </div>
           )
