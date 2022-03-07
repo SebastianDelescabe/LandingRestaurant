9c39419e202e3ec9bbcf64e9ae177ebb3e5aa607
Sebastian <sebastiandelescabe@gmail.com>
2022-03-07 18:44:59 -0300

    chef section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         <li className='p__opensans'><a href='#home'>Home</a></li>
         <li className='p__opensans'><a href='#about'>About</a></li>
         <li className='p__opensans'><a href='#menu'>Menu</a></li>
-        <li className='p__opensans'><a href='#awards'>Awards</a></li>
+        <li className='p__opensans'><a href='#gallery'>Gallery</a></li>
         <li className='p__opensans'><a href='#contact'>Contact</a></li>
       </ul>
       <div className='app__navbar-login'>
@@ -37,7 +37,7 @@ const Navbar = () => {
                 <li className='p__opensans'><a href='#home'>Home</a></li>
                 <li className='p__opensans'><a href='#about'>About</a></li>
                 <li className='p__opensans'><a href='#menu'>Menu</a></li>
-                <li className='p__opensans'><a href='#awards'>Awards</a></li>
+                <li className='p__opensans'><a href='#gallery'>Gallery</a></li>
                 <li className='p__opensans'><a href='#contact'>Contact</a></li>
               </ul>
             </div>
